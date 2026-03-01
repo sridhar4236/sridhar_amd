a547fa6a465f11edf76361f363a8d2ba2170190d
BEESAM SRIDHAR SAGAR <sridharsagar360@gmail.com>
2026-03-01 07:42:07 +0530

    first commit

---
+++ helloworld.c
@@ -0,0 +1,19 @@
+/********************************************
+* Name         : B SRIDHAR SAGAR
+* KMID         : KM62AEAI04
+* Date         : 01-03-26
+* Program      :
+* sample Input :
+* Sample Output:   
+* 
+***************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+	printf("first hello world \n");
+
+	return 0;
+} 
+
